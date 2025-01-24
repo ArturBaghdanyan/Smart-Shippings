5fb28f52e2f0f84a110505bd908735b4bd50b034
ArturBaghdanyan <arturbaghdanyan20@gmail.com>
2025-01-24 16:51:50 +0400

    first commit

---
+++ data.js
@@ -0,0 +1,138 @@
+export const swiperData = [
+    {
+        id: 1,
+        bgImage: '../assets/swiper/airplane.jpg',
+        text1: 'accelerate',
+        text2: 'your freight with',
+        text3: 'air freight!',
+        text4: 'trust us your air freight forwarding, it\'s fast, <br> easy and convenient with us',
+        button: 'Read More'
+    },
+    {
+        id: 2,
+        bgImage: '../assets/swiper/truck.jpg',
+        text1: 'SEA FREIGHTS',
+        text2: 'Global Solutions For Your Business',
+        text3: 'Are you looking for a cost-effective freight forwarding service,then our sea freight service is for YOU!',
+        button: 'Read More',
+    },
+    {
+        id: 3,
+        bgImage: '../assets/swiper/ship.jpg',
+        text1: 'ROAD FREIGHT',
+        text2: 'Shortens Transit Times',
+        text3: 'Choose our road freight service for your freight forwarding. It\'s fast, flexible and cost-effective.',
+        text4: 'trust us your air freight forwarding, it\'s fast,<br>  easy and convenient with us',
+        button: 'Read More',
+    },
+]
+
+export const sectionData = [
+    {
+        id: 1,
+        icon: '../assets/section/airplane-mode.svg',
+        title: 'Air Freight',
+        text: 'Our Air Freight Services Include: complete Logistics Programs (pick,…',
+        button: 'READ MORE'
+    },
+    {
+        id: 2,
+        icon: '../assets/section/ship.svg',
+        title: 'Sea Freight',
+        text: 'Our Sea Freight Services Include: FCL and LCL import and export, …',
+        button: 'READ MORE'
+    },
+    {
+        id: 3,
+        icon: '../assets/section/rail.svg',
+        title: 'Rail Freight',
+        text: 'When rail freight is the best option available. When shipping inland…',
+        button: 'READ MORE'
+    },
+    {
+        id: 4,
+        icon: '../assets/section/road.svg',
+        title: 'Road Freight',
+        text: 'Our Road Transportation Services Include: transportation of LTL, FTL…',
+        button: 'READ MORE'
+    },
+    {
+        id: 5,
+        icon: '../assets/section/cash-dollar.svg',
+        title: 'Customs Brokerage',
+        text: 'Customs Services include: import/export customs clearance service.…',
+        button: 'READ MORE'
+    },
+    {
+        id: 6,
+        icon: '../assets/section/check.svg',
+        title: 'Cargo Insurance',
+        text: 'Our Cargo Insurance service includes: "All Risk" cargo and marine insurance, auto…',
+        button: 'READ MORE'
+    },
+    {
+        id: 7,
+        icon: '../assets/section/box.svg',
+        title: 'Warehousing',
+        text: 'Our Warehousing Services include: short and long term storage, cargo…',
+        button: 'READ MORE'
+    },
+    {
+        id: 8,
+        icon: '../assets/section/settings.svg',
+        title: 'Project Logistics',
+        text: 'Our Project Logistics Services Includes: project logistics feasibility…',
+        button: 'READ MORE'
+    },
+];
+
+export const asideSwiperList = [
+    {
+        id: 1,
+        header: 'High Customer Service',
+        about: 'I’ve been happy with the services provided by Smart Shippings LLC. They ha e returned my calls quickly, and answered all my questions!',
+        info: [
+            {
+                icon: '../assets/aside/first-icon.png',
+                title: 'Armine Hovhannisyan',
+                text: 'Yerevan'
+            }
+        ],
+    },
+    {
+        id: 2,
+        header: 'Highly Recommend',
+        about: 'I am happy to find Smart Shippings. They are trustful and have good customer service. So I highly recommend them.',
+        info: [
+            {
+                icon: '../assets/aside/sec-icon.png',
+                title: 'Anna Martirosyan',
+                text: 'Yerevan'
+            }
+        ],
+    },
+    {
+        id: 3,
+        header: 'Easy and Reliable',
+        about: 'We were previously using an unreliable company with a very complicated registration and transfer process. Then we found Smart Logistics and thats the…',
+        info: [
+            {
+                icon: '../assets/aside/th-icon.png',
+                title: 'Armen Karapetyan',
+                text: 'Yerevan'
+            }
+        ],
+    },
+    {
+        id: 4,
+        header: 'Excellent Service',
+        about: 'Excellent service, competitive pricing and customer support. They know what their clients need.',
+        info: [
+            {
+                icon: '../assets/aside/fr-icon.png',
+                title: 'Ani Harutyunyan',
+                text: 'Yerevan'
+            }
+        ],
+    },
+];
---
+++ index.js
@@ -0,0 +1,182 @@
+import { swiperData, sectionData, asideSwiperList } from "./data.js";
+
+$(document).ready(function() {
+
+    const swiper1 = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1,
+        slidesPerGroup: 1,  
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
+
+$(document).ready(function() {
+    $(".text").on("blur", function () {
+        $(".text").css("color", "");
+        $(".text2, .text3, .text4, .text5, .text6").css("color", "");
+    });
+    
+    
+    $("#desktop-nav a").on('click', function () {
+        $("#desktop-nav a").css("color", "");
+        $(this).css('color', 'blue');
+    });
+});
+
+$(document).ready(function () {
+    $(".bx-search").on('click', function () {
+        $("#search-icon").toggle('slow');
+        console.log('clicked');
+    });
+
+    $(".dropdown-toggle").hover(
+        function () {
+            $(".dropdown-menu").addClass("show");
+        },
+        function () {
+            $(".dropdown-menu").removeClass("show");
+        }
+    );
+    });
+$(document).ready(function() {
+
+    const swiperWrapper = $(".swiper-wrapper");
+
+    function isInViewport(element) {
+        var top = $(window).scrollTop();
+        var offset = $(element).offset().top - 150;
+        var height = $(element).outerHeight();
+        return top >= offset && top <= offset + height;
+    }
+
+    // Function to handle scroll event
+    function handleScroll() {
+        $('.columns').each(function() {
+            if (isInViewport(this)) {
+                $(this).addClass('show-animate');
+            } else {
+                $(this).removeClass('show-animate');
+            }
+        });
+    }
+
+    // Initial check on page load
+    handleScroll();
+
+    // Event listener for scroll
+    $(window).scroll(function() {
+        handleScroll();
+    });
+
+    swiperData.forEach(item => {
+
+        const slideHTML = `    
+      <div class="swiper-slide columns" key=${item.id}
+      style="background-image: url('${item.bgImage}')">
+        <div class="column size">
+            <div class="lh-md d-flex flex-column swiper-image" 
+                style="${item === swiperData[0] ? 'text-transform: uppercase;' : ''}">
+                <span style="font-size: 64px" class="animate">${item.text1}</span>
+                <span class="fs-2 fw-bold animate">${item.text2}</span>
+                <span 
+                style="padding-left: 33rem; ${item === swiperData[0] ? 'font-size: 64px;' : 'font-size: 18px'}"
+                class="animate"
+                >${item.text3}</span>
+            </div>
+            ${item === item.text4 ? `
+              <div class="animate mt-3" style="font-size: 18px">
+                  ${item.text4}
+              </div>
+            ` : ''}
+            
+            <button 
+              class="w-auto p-3 bg-opacity-10 opacity-1 mt-4 border-0" 
+              style="background: #2a2c32cc; border-radius: 3px">
+                <a href="/" class="text-decoration-none text-white">${item.button}</a>
+            </button>
+        </div>
+      </div>
+      `;
+        swiperWrapper.append(slideHTML);
+})
+});
+$(document).ready(function() {
+
+    const sectionWrapper = $('#section-wrapper');
+    sectionData.forEach(item => {
+
+        const listHTML = `
+      <div class="section-piece">
+        <div class="d-flex flex-column align-items-start w-100 h-100 gap-3" style="padding: 2rem">
+        <img src="${item.icon}" alt="icon" style="width: 30px; height: 30px">
+          <h2 style="font-weight: 600; font-size: 24px;">${item.title}</h2>
+          <p style="font-size: 14px">${item.text}</p>
+          <button class="border-0" style="background: none">
+            <a href="#" class="text-decoration-none fw-bold text-#038ed3">
+              ${item.button}
+            </a>
+          </button>
+        </div>
+      </div>
+    `;
+        sectionWrapper.append(listHTML);
+    });
+});
+
+const asideWrapper = $('.aside-wrapper');
+const asideSwiperListToShow = asideSwiperList.slice(0, 4);
+
+asideSwiperListToShow.forEach(item => {
+    
+    let itemsHTML = '';
+    item.info.forEach(i => {
+         itemsHTML += `
+            <div class="d-flex gap-3 w-100">
+            <div style="width: 80px; height: 80px">
+                <img src="${i.icon}" alt="icon" style="width: 100%; height: 100%;">
+            </div>
+            <div class="d-flex flex-column justify-content-center gap-1">
+                <span>${i.title}</span>
+                <p>${i.text}</p>
+            </div>
+            </div>
+        `
+    });
+    const asideHTML = `
+    <div key=${item.id} class="swiper-slide">
+        ${(item <= 4) ? 
+            `<div class="column">
+            <h2 class="fw-bold">${item.header}</h2>
+            <p style="font-size: 14px">${item.about}</p>
+            <div>${itemsHTML}</div>
+            </div>`
+        : ''}
+        
+    </div>
+    `;
+
+asideWrapper.append(asideHTML);
+});
+
+$(document).ready(function() {
+    $(".show-text").on('click', function() {
+        $(this).closest('.accordion-item').find('.panel-text').toggle('slow');
+        let img = $(this).find('img');
+
+
+        img.attr('src', function(index, oldSrc) {
+            return oldSrc.endsWith('plus.svg') ? '../assets/article/minus.svg' : '../assets/article/plus.svg';
+        });
+
+    });
+    $('.panel-text').hide();
+});
